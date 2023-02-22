08d874b05a7ab3d9c04339f4ea12f5f0bfc6f3e6
Moolshankar Tyagi <moolshankartyagi@gmail.com>
2023-02-22 20:49:25 +0530

    Create setup.sh

---
+++ image_processing_v1/server/setup.sh
@@ -0,0 +1,14 @@
+#mkvirtualenv image_processing -p python3
+pip3 install virtualenv
+virtualenv image-processing-env
+source image-processing-env/bin/activate
+pip3 install -r requirements.txt
+# pip3 install opencv-contrib-python
+# pip3 install cmake
+# pip3 install dlib
+# pip3 install face_recognition
+# pip3 install pickle-mixin
+# pip3 install imutils
+# pip3 install numpy
+# pip3 install flask
+# pip3 install -U flask-cors
